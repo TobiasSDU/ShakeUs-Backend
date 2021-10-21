dac1705ae3715017eee0601c6b0dfe3a3959a753
Tobias Kristensen <tobiaskristensen12@gmail.com>
2021-10-21 13:51:06 +0200

    Add Guest model

---
+++ test/models/Guest.test.ts
@@ -0,0 +1,26 @@
+import { Guest } from './../../src/models/Guest';
+
+const testName = 'TestGuest';
+const originalGuest = new Guest(testName);
+
+describe('party model methods return expected values', () => {
+    let testGuest: Guest;
+
+    beforeEach(() => {
+        testGuest = Object.assign(
+            Object.getPrototypeOf(originalGuest),
+            originalGuest
+        );
+    });
+
+    it('returns a vaild uuid', () => {
+        const uuidFormat =
+            /\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b/;
+
+        expect(testGuest.id).toMatch(uuidFormat);
+    });
+
+    it('returns the correct test name', () => {
+        expect(testGuest.name).toEqual(testName);
+    });
+});
