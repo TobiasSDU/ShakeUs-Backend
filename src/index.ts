f105360c30be0640f0e5077c6e3b8aeea3df246c
Tobias Kristensen <tobiaskristensen12@gmail.com>
2021-10-25 12:49:57 +0200

    Setup dummy client files

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,14 @@ import { partyRoutes } from './routes/party_routes';
 import { guestRoutes } from './routes/guest_routes';
 import { activityPackRoutes } from './routes/activity_pack_routes';
 import { activityRoutes } from './routes/activity_routes';
+import path from 'path';
 
 export const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, '../dummy-client/')));
 
 app.use('/party', partyRoutes);
 app.use('/guest', guestRoutes);
