187cdfaa08583cbe9b41ccea62dabcbb0f98e613
Tobias Kristensen <tobiaskristensen12@gmail.com>
2021-10-26 11:57:28 +0200

    Add template activity pack with activities

--- src/templates/default_activities.ts
+++ src/templates/default_activities.ts
@@ -1,3 +1,28 @@
 import { Activity } from '../models/activity';
 
-export const defaultActivities: Activity[] = [];
+export const defaultActivities: Activity[] = [
+    new Activity(
+        'DefaultActivity4',
+        'Activity4',
+        'Activity4Description',
+        Date.now()
+    ),
+    new Activity(
+        'DefaultActivity4',
+        'Activity4',
+        'Activity4Description',
+        Date.now()
+    ),
+    new Activity(
+        'DefaultActivity4',
+        'Activity4',
+        'Activity4Description',
+        Date.now()
+    ),
+    new Activity(
+        'DefaultActivity4',
+        'Activity4',
+        'Activity4Description',
+        Date.now()
+    ),
+];
--- src/templates/default_activity_packs.ts
+++ src/templates/default_activity_packs.ts
@@ -1,3 +1,16 @@
 import { ActivityPack } from '../models/activity_pack';
+import { defaultActivities } from './default_activities';
 
-export const defaultActivityPacks: ActivityPack[] = [];
+const defaultActivityPack = new ActivityPack(
+    'DefaultActivityPack1',
+    'ActivityPack1',
+    'ActivityPack1Description',
+    [
+        defaultActivities[0].id,
+        defaultActivities[1].id,
+        defaultActivities[2].id,
+        defaultActivities[3].id,
+    ]
+);
+
+export const defaultActivityPacks: ActivityPack[] = [defaultActivityPack];
