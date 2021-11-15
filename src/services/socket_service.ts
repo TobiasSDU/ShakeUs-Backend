47a03e0e74ed226c45c1942ff5776b6c472f1a51
Tobias Kristensen <tobiaskristensen12@gmail.com>
2021-11-15 10:17:41 +0100

    Test Socket.IO emit from client

--- src/services/socket_service.ts
+++ src/services/socket_service.ts
@@ -7,16 +7,14 @@ export class SocketService {
     constructor(server: http.Server) {
         this.io = new Server(server);
 
-        this.io.on('HEJ', (socket) => {
-            console.log(socket.id + " sagde hej");
-        })
-
         this.io.on('connection', (socket) => {
             console.log("Socket connected: " + socket.id);
             socket.on('join-room', (roomId) => {
                 socket.join(roomId);
                 console.log('User joined room: ' + roomId);
             });
+
+            socket.emit("Davs", "Ay");
 
             socket.on('leave-room', (roomId) => {
                 socket.leave(roomId);
